1a6c176c9237b2411c07df730674c24d7a0c0c6d
YNNCK-V <vandevenne21@gmail.com>
2021-10-10 22:55:26 +0200

    Added PrettyTable

--- marvel.py
+++ marvel.py
@@ -3,13 +3,14 @@
 # Necessary modules: send HTTP requests; hash messages
 import requests
 import hashlib
+from prettytable import PrettyTable
 
 # API Link
 url = "http://gateway.marvel.com/v1/public/characters?"
 # Public key (use your own)
-public_key = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+public_key = "3d841a427299ab59553e2072785a7de6"
 # Private key (use your own)
-private_key = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+private_key = "bfd021ba167240818a0212094dcde76f71fb5026"
 # Timestamp
 timestamp = "1"
 # Hash, is used in URL when getting the data
@@ -22,6 +23,9 @@ while True:
     # If the input is 'q', break out of loop
     if name == "q":
         break
+    elif name == "":
+        print("Please enter a name.")
+        continue
     # Send request with parameters, store response in variable 'data'
     data = requests.get(url, params={
         "name": name,
@@ -37,10 +41,30 @@ while True:
             print("Marvel character not found, try again: ")
         # If the character DOES exist, print and parse everything requested from the JSON
         else:
-            print("NAME: " + name)
+            # PrettyTable for character's name
+            nameDescTable = PrettyTable()
+            nameDescTable.field_names = ["Character Name",]
+            name = data["data"]["results"][0]["name"]
+            nameDescTable.add_row([name])
+            print(nameDescTable)
             print("DESCRIPTION:\n" + str(data["data"]["results"][0]["description"]))
-            print("ID: " + str(data["data"]["results"][0]["id"]) + "\n " + "-" * 30)
-            print("\nSERIES:")
+
+            # PrettyTable for comics
+            comicsTable = PrettyTable()
+            comicsTable.field_names = ["Marvel Comics Featuring " + name]
+             # For each character's name that can be found, add row
+            for each in data["data"]["results"][0]["comics"]["items"]:
+                comicsTable.add_row([each["name"],])
+            print(comicsTable)
+
+            # PrettyTable for series
+            seriesTable = PrettyTable()
+            seriesTable.field_names = ["Marvel Comic Book Series Featuring " + name]
+             # For each character's name that can be found, add row
             for each in data["data"]["results"][0]["series"]["items"]:
-                print(" - " + each["name"])
+                seriesTable.add_row([each["name"],])
+            
+            print(seriesTable)
+
+            # Legal stuff
             print("\n " + str(data["attributionText"]) + "\n ")
